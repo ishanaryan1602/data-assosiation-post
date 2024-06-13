4b12b7a4952bdb9ebab122f25913556b476d1399
ishanaryan1602 <github1602@gmail.com>
2024-06-13 20:15:02 +0530

    mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const userModel = require('./models/user');
+const postModel = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/data-ass').then(()=>console.log('succesfully connected to MonogDB')).catch((err)=>console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
